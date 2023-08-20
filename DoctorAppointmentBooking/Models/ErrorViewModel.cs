8ab7bb5275f706c15005fc5fa0f1743598cf3ef3
hackathon <hackathon@Aparna-VM>
2023-08-20 18:19:20 +0000

    Add project files.

---
+++ DoctorAppointmentBooking/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DoctorAppointmentBooking.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
